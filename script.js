c193d6f82a6a6735ebfab4bd3daf61b4afcc401b
Ion Moraru <moraru.ion767@gmail.com>
2025-04-12 21:03:55 +0300

    Add createGrid function

--- script.js
+++ script.js
@@ -1,34 +1,12 @@
 const container = document.querySelector('#container');
 container.setAttribute('class', 'grid');
 
-for (let i = 1; i <= 16; i++) {
-    const rows = document.createElement('div');
-    rows.setAttribute('class', 'boxRow');
-    container.appendChild(rows);
-    for (let j = 1; j <= 16; j++) {
-        const cell = document.createElement('div');
-        cell.setAttribute('class', 'gridElement');
-        cell.style.height = String(850 / 16) + "px";
-        cell.style.width = String(850 / 16) + "px";
-        rows.appendChild(cell);
-        cell.addEventListener('mouseover', () => {
-        cell.setAttribute('class', 'gridElementChanged')
-        })
-    }
-    
-}
-
-
 const button = document.createElement('button');
 button.textContent = 'Change Grid Dimensions';
 button.setAttribute('class', 'button')
 document.body.appendChild(button)
 
-button.addEventListener('click', () => {
-    let row = prompt('How many rows do you want to set?');
-    let column = prompt('How many columns do you want to set?');
-    container.innerHTML = '';
-
+function createGrid(row, column) {
     for (let i = 1; i <= row; i++) {
         const rows = document.createElement('div');
         rows.setAttribute('class', 'boxRow');
@@ -40,8 +18,17 @@ button.addEventListener('click', () => {
             cell.style.width = String(850 / column) + "px";
             rows.appendChild(cell);
             cell.addEventListener('mouseover', () => {
-            cell.setAttribute('class', 'gridElementChanged')
-            
-        })
+            cell.setAttribute('class', 'gridElementChanged');
+            })
+        }
     }
-}})
+}
+
+button.addEventListener('click', () => {
+    let desiredRow = prompt('How many rows do you want to set?');
+    let desiredColumn = prompt('How many columns do you want to set?');
+    container.innerHTML = '';
+    createGrid(desiredRow, desiredColumn);
+})
+
+createGrid(16, 16);
