01d8b04987ab7bdd0e0e132d751076a4a3e8f2de
Ion Moraru <moraru.ion767@gmail.com>
2025-04-12 20:48:16 +0300

    Add grid based on user input

--- script.js
+++ script.js
@@ -1,17 +1,23 @@
 const container = document.querySelector('#container');
 container.setAttribute('class', 'grid');
 
-
 for (let i = 1; i <= 16; i++) {
+    const rows = document.createElement('div');
+    rows.setAttribute('class', 'boxRow');
+    container.appendChild(rows);
     for (let j = 1; j <= 16; j++) {
-        const div = document.createElement('div');
-        div.setAttribute('class', 'gridElement');
-        div.addEventListener('mouseover', () => {
-            div.setAttribute('class', 'gridElementChanged');
+        const cell = document.createElement('div');
+        cell.setAttribute('class', 'gridElement');
+        cell.style.height = String(850 / 16) + "px";
+        cell.style.width = String(850 / 16) + "px";
+        rows.appendChild(cell);
+        cell.addEventListener('mouseover', () => {
+        cell.setAttribute('class', 'gridElementChanged')
         })
-        container.appendChild(div);
+    }
+    
 }
-}
+
 
 const button = document.createElement('button');
 button.textContent = 'Change Grid Dimensions';
@@ -23,15 +29,19 @@ button.addEventListener('click', () => {
     let column = prompt('How many columns do you want to set?');
     container.innerHTML = '';
 
-    for (let i = 1; i<=row; i++) {
-        for (let j = 1; j<=column; j++){
-            const div = document.createElement('div');
-            div.setAttribute('class', 'gridElement');
-            div.addEventListener('mouseover', () => {
-            div.setAttribute('class', 'gridElementChanged');
-            })
-            container.appendChild(div);
-    }}
-})
-
-
+    for (let i = 1; i <= row; i++) {
+        const rows = document.createElement('div');
+        rows.setAttribute('class', 'boxRow');
+        container.appendChild(rows);
+        for (let j = 1; j <= column; j++) {
+            const cell = document.createElement('div');
+            cell.setAttribute('class', 'gridElement');
+            cell.style.height = String(850 / row) + "px";
+            cell.style.width = String(850 / column) + "px";
+            rows.appendChild(cell);
+            cell.addEventListener('mouseover', () => {
+            cell.setAttribute('class', 'gridElementChanged')
+            
+        })
+    }
+}})
