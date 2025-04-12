87c1b63d02e88567c6dd9776b5816bae26111a15
Ion Moraru <moraru.ion767@gmail.com>
2025-04-12 15:41:55 +0300

    Set hover effect so that the grid element changes color when mouse passes over it.

--- script.js
+++ script.js
@@ -5,6 +5,10 @@ container.setAttribute('class', 'grid')
 for (let i = 1; i <= 256; i++) {
     const div = document.createElement('div');
     div.setAttribute('class', 'gridElement')
+    div.addEventListener('mouseover', () => {
+        div.setAttribute('class', 'gridElementChanged')
+    })
     container.appendChild(div)
 }
+
 
