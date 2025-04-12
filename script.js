1ecafa49b20be93fa23c9b76129ced28459681aa
Ion Moraru <moraru.ion767@gmail.com>
2025-04-12 17:22:47 +0300

    Add change grid button

--- script.js
+++ script.js
@@ -1,14 +1,37 @@
-const container = document.querySelector('#container')
-container.setAttribute('class', 'grid')
+const container = document.querySelector('#container');
+container.setAttribute('class', 'grid');
 
 
-for (let i = 1; i <= 256; i++) {
-    const div = document.createElement('div');
-    div.setAttribute('class', 'gridElement')
-    div.addEventListener('mouseover', () => {
-        div.setAttribute('class', 'gridElementChanged')
-    })
-    container.appendChild(div)
+for (let i = 1; i <= 16; i++) {
+    for (let j = 1; j <= 16; j++) {
+        const div = document.createElement('div');
+        div.setAttribute('class', 'gridElement');
+        div.addEventListener('mouseover', () => {
+            div.setAttribute('class', 'gridElementChanged');
+        })
+        container.appendChild(div);
 }
+}
+
+const button = document.createElement('button');
+button.textContent = 'Change Grid Dimensions';
+button.setAttribute('class', 'button')
+document.body.appendChild(button)
+
+button.addEventListener('click', () => {
+    let row = prompt('How many rows do you want to set?');
+    let column = prompt('How many columns do you want to set?');
+    container.innerHTML = '';
+
+    for (let i = 1; i<=row; i++) {
+        for (let j = 1; j<=column; j++){
+            const div = document.createElement('div');
+            div.setAttribute('class', 'gridElement');
+            div.addEventListener('mouseover', () => {
+            div.setAttribute('class', 'gridElementChanged');
+            })
+            container.appendChild(div);
+    }}
+})
 
 
